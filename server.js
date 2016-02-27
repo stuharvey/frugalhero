d63d9b6ecfd9808ea23a366bf0ddb45a348b12a6
Hunter Morgan <automaticgiant@gmail.com>
2016-02-27 12:36:35 -0500

    merge/mod

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-
 var fs = require('fs');
 var request = require('superagent');
 var http = require('http');
 var express = require('express');
-
+var path = require('path');
 var port = process.env.PORT || 3000;
 var CAPITALONE_KEY = fs.readFileSync('server_keys/capitalone_key');
 var server = express();
 
-server.get('/', get);
-
-/**
- * Handle / gets
- * @param  {Object} req Get request
- * @param  {Object} res Response to send
- */
-function get(req, res) {
-  request
-    .get('http://api.reimaginebanking.com/atms?key=' + CAPITALONE_KEY)
-    .end(function(err, c1Response) {
-      // console.log(res.status);
-      console.log(c1Response.body); // Write capital one's response to console
-      res.end(JSON.stringify(c1Response.body));
-  });
-}
+server.use(express.static('public'));
 
 server.get('/config/:uid', getConfig);
 server.put('/config/:uid', storeConfig);
@@ -34,6 +18,5 @@ function storeConfig(req, res) {
 }
 
 http.createServer(server).listen(port);
-app.use(express.static('public'));
 
 console.log("Listening on port: " + port);
