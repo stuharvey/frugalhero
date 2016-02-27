bdb1282dd44697a358dbece9fbb5141739414425
stuharvey <stu@vt.edu>
2016-02-26 22:23:53 -0500

    Send some data from the CapitalOne API to our site

--- server.js
+++ server.js
@@ -2,19 +2,29 @@
 var fs = require('fs');
 var request = require('superagent');
 var http = require('http');
+var express = require('express');
 
+var port = process.env.PORT || 3000;
 var CAPITALONE_KEY = fs.readFileSync('server_keys/capitalone_key');
+var server = express();
 
-request
-  .get('http://api.reimaginebanking.com/atms?key=' + CAPITALONE_KEY)
-  .end(function(err, res) {
-    // console.log(res.status);
-    console.log(err);
-    console.log(res.body); //do something
-});
+server.get('/', get);
 
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+/**
+ * Handle / gets
+ * @param  {Object} req Get request
+ * @param  {Object} res Response to send
+ */
+function get(req, res) {
+  request
+    .get('http://api.reimaginebanking.com/atms?key=' + CAPITALONE_KEY)
+    .end(function(err, c1Response) {
+      // console.log(res.status);
+      console.log(c1Response.body); // Write capital one's response to console
+      res.end(JSON.stringify(c1Response.body));
+  });
+}
+
+http.createServer(server).listen(port);
+
+console.log("Listening on port: " + port);
