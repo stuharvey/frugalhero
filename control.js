a617141075dcfd3f02559284d91cf7a2ae288deb
stuharvey <stu@vt.edu>
2016-02-27 18:50:52 -0500

    Server now watches nest for updates

--- control.js
+++ control.js
@@ -1,30 +1,55 @@
 var TempControl = module.exports = {};
 
 var EventSource = require('eventsource');
+var log = require('debug-logger')('hacktheplanet');
+
 // This API will emit events from this URL.
 var NEST_API_URL = 'https://developer-api.nest.com';
+
+var thermostat = {},
+    structure = {};
 
 TempControl.start = function(token) {
   var source = new EventSource(NEST_API_URL + '?auth=' + token);
 
-  source.addEventListener('put', function(e) {
-    console.log('\n' + e.data);
-  });
+  source.addEventListener('put', onDataFromNest);
 
   source.addEventListener('open', function(e) {
-    console.log('Connection opened!');
+    log.info('Connection opened!');
   });
 
   source.addEventListener('auth_revoked', function(e) {
-    console.log('Authentication token was revoked.');
+    log.info('Authentication token was revoked.');
     // Re-authenticate your user here.
   });
 
   source.addEventListener('error', function(e) {
     if (e.readyState == EventSource.CLOSED) {
-      console.error('Connection was closed! ', e);
+      log.error('Connection was closed! ', e);
     } else {
-      console.error('An unknown error occurred: ', e);
+      log.error('An unknown error occurred: ', e);
     }
   }, false);
 };
+
+function onDataFromNest(snapshot) {
+  var data = JSON.parse(snapshot.data).data;
+  structure  = firstChild(data.structures);
+  thermostat = data.devices.thermostats[structure.thermostats[0]];
+  log.info('Got an update from the thermometer, new target temperature is ',
+    thermostat.target_temperature_f);
+}
+
+/**
+  Utility method to return the first child
+  value of the passed in object.
+
+  @method
+  @param object
+  @returns object
+*/
+function firstChild(object) {
+  for(var key in object) {
+    return object[key];
+  }
+}
