61712fbd31a83b24e4165cf330e48c420e418467
stuharvey <stu@vt.edu>
2016-02-28 09:36:40 -0500

    Get updates from server on frontend

--- public/ui/static/js/main.js
+++ public/ui/static/js/main.js
@@ -1,16 +1,59 @@
-/*global $, jQuery*/
-//$(document).ready(function () {
-//    "use strict";
-//    $("button").on("click", function (e) {
-//        if ($(this).hasClass('active')) {
-//            $(this).removeClass('active');
-//            $("#health-bar").css({"width": "20%", "color": "red"});
-//        } else {
-//            $(this).addClass('active');
-//            $("#health-bar").css({"width": "70%", "color": "green"});
-//        }
-//    });
-//});
+$.get('/status', function(user) {
+  var goals = user.goals;
+  if (goals.indexOf('avoidFees') !== -1) {
+    if (!$("#atm-skill").hasClass('active') && !user.gotATMFee) {
+      $("#atm-skill").addClass('active');
+    }
+    else if ($("#atm-skill").hasClass('active') && user.gotATMFee) {
+      $(".health-bar-value").css({"width": "30%"});
+      $("#atm-skill").removeClass('active');
+    }
+  }
+
+  if (goals.indexOf('eatathome') !== -1) {
+    if (!$("#eat-out").hasClass('active') && !user.ateOut) {
+      $("#eat-out").addClass('active');
+    }
+    else if ($("#eat-out").hasClass('active') && user.ateOut) {
+      $(".health-bar-value").css({"width": "30%"});
+      $("#eat-out").removeClass('active');
+    }
+
+  }
+  if (goals.indexOf('alcatgrocery') !== -1) {
+    if (!$("#liquor").hasClass('active') && !user.wentToBar) {
+      $("#liquor").addClass('active');
+    }
+    else if ($("#liquor").hasClass('active') && user.wentToBar) {
+      $("#liquor").removeClass('active');
+    }
+
+  }
+  if (goals.indexOf('paybillsontime') !== -1) {
+    if (!$("#bills-skill").hasClass('active') && !user.lateOnBill) {
+      $("#bills-skill").addClass('active');
+    }
+    else if ($("#bills-skill").hasClass('active') && user.lateOnBill) {
+      $(".health-bar-value").css({"width": "30%"});
+      $("#bills-skill").removeClass('active');
+    }
+
+  }
+  if (goals.indexOf('spendsave') !== -1) {
+    if (!$("#savings").hasClass('active') && !user.belowQuota) {
+      $("#savings").addClass('active');
+    }
+    else if ($("#savings").hasClass('active') && user.belowQuota) {
+      $(".health-bar-value").css({"width": "30%"});
+      $("#savings").removeClass('active');
+    }
+
+  }
+
+  // if (user.gotATMFee) {
+  //   if($("#atm-skill").hasClass('active'))
+  // }
+});
 
 
 $(document).ready(function () {
